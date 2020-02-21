93899f3f8925329c02dbaf58ddd87f42387f8bf9
Adán Fernández <47212287+adanvetusta@users.noreply.github.com>
2020-02-21 19:37:54 +0100

    Definición de componentes iniciales

--- adminpro/src/app/app.module.ts
+++ adminpro/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+// Rutas
+import { APP_ROUTES } from './app.routes';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -11,6 +12,8 @@ import { Graficas1Component } from './pages/graficas1/graficas1.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { PagesComponent } from './pages/pages.component';
+
 
 @NgModule({
   declarations: [
@@ -22,10 +25,12 @@ import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component
     Graficas1Component,
     HeaderComponent,
     SidebarComponent,
-    BreadcrumbsComponent
+    BreadcrumbsComponent,
+    PagesComponent
   ],
   imports: [
     BrowserModule,
+    APP_ROUTES,
     AppRoutingModule
   ],
   providers: [],
---
+++ adminpro/src/app/app.routes.ts
@@ -0,0 +1,39 @@
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Graficas1Component } from './pages/graficas1/graficas1.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { PagesComponent } from './pages/pages.component';
+const appRoutes: Routes = [
+    //Cualquier ruta vacía hará un redirect to al dashboard
+    {
+        path: '', component: PagesComponent,
+        children: [
+            {
+                path: 'progress', component: ProgressComponent
+            },
+            {
+                path: 'graficas1', component: Graficas1Component
+            },
+            {
+                path: 'dashboard', component: DashboardComponent
+            },
+            {
+                path: '', redirectTo: '/dashboard', pathMatch: 'full'
+            }
+        ]
+    },
+    {
+        path: 'login', component: LoginComponent
+    },
+    {
+        path: 'register', component: LoginComponent
+    },
+    //Cualquier ruta no definida irá al nopagefoundcomponent
+    {
+        path: '**', component: NopagefoundComponent
+    }
+];
+
+export const APP_ROUTES = RouterModule.forRoot(appRoutes, { useHash: true });
