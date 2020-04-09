686419469ec30a7362a95c46cfb671d3927bf988
Adán Fernández <47212287+adanvetusta@users.noreply.github.com>
2020-04-09 22:27:18 +0200

    Añadiendo metadatos a la página
    Podemos añadir más metadatos añadiéndolos donde hemos añadido el título en cada componente.

--- adminpro/src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ adminpro/src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivationEnd } from '@angular/router';
 import { filter, map } from 'rxjs/operators';
+import { Title, Meta, MetaDefinition } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -11,10 +12,19 @@ export class BreadcrumbsComponent implements OnInit {
 
   titulo: string;
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private title: Title, private meta: Meta) {
     this.getDataRoute()
     .subscribe( data => {
       this.titulo = data.titulo;
+
+      // Titulo de la pestaña
+      this.title.setTitle(this.titulo);
+
+      const metaTag: MetaDefinition = {
+        name: 'description',
+        content: this.titulo
+      };
+      this.meta.updateTag(metaTag);
     });
    }
 
@@ -25,9 +35,12 @@ export class BreadcrumbsComponent implements OnInit {
     return this.router.events.pipe(
       // Solo nos interesan los que sean una instancia del ActivationEnd
       filter(evento => evento instanceof ActivationEnd),
+
       // Solo nos interesa el que tenga el firstChild igual a null (entiendo que es porque no tiene hijos y es en el que se está)
+
       filter( (evento: ActivationEnd) => evento.snapshot.firstChild === null),
       // Solo quiero extraer la data (como un aspersor de agua)
+
       map( (evento: ActivationEnd) => evento.snapshot.data)
     );
   }
