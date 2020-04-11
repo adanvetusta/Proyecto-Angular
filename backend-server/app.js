a112d89fccae327f8e552e329178e48786714183
Adán Fernández <47212287+adanvetusta@users.noreply.github.com>
2020-04-11 22:48:15 +0200

    Operaciones CRUD: hospital

--- backend-server/app.js
+++ backend-server/app.js
@@ -25,8 +25,11 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) =
 var appRoutes = require('./routes/app');
 var usuarioRoutes = require('./routes/usuario');
 var loginRoutes = require('./routes/login');
+var hospitalRoutes = require('./routes/hospital');
+
 
 // Rutas
+app.use('/hospital', hospitalRoutes);
 app.use('/usuario', usuarioRoutes);
 app.use('/login', loginRoutes);
 app.use('/', appRoutes);
