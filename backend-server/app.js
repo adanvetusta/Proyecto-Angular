900ac7570dded851709e577614b84516a7906376
Adán Fernández <47212287+adanvetusta@users.noreply.github.com>
2020-04-12 21:56:46 +0200

    Búsqueda con promesas

--- backend-server/app.js
+++ backend-server/app.js
@@ -27,6 +27,7 @@ var usuarioRoutes = require('./routes/usuario');
 var loginRoutes = require('./routes/login');
 var hospitalRoutes = require('./routes/hospital');
 var medicoRoutes = require('./routes/medico');
+var busquedaRoutes = require('./routes/busqueda');
 
 
 
@@ -35,6 +36,7 @@ app.use('/medico', medicoRoutes);
 app.use('/hospital', hospitalRoutes);
 app.use('/usuario', usuarioRoutes);
 app.use('/login', loginRoutes);
+app.use('/busqueda', busquedaRoutes);
 app.use('/', appRoutes);
 
 // Escuchar peticiones
---
+++ backend-server/routes/busqueda.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var app = express();
+
+var Hospital = require('../models/hospital');
+var Medico = require('../models/medico');
+
+app.get('/todo/:busqueda', (req, res, next) => {
+    var busqueda = req.params.busqueda;
+
+    // que contenga norte y sea insensible a min y may: /busqueda/i.
+    var regex = new RegExp(busqueda, 'i');
+
+    buscarHospitales(regex).then(hospitales => {
+        res.status(200).json({
+            ok: true,
+            hospitales: hospitales
+        });
+    });
+});
+
+
+function buscarHospitales(regex) {
+
+    return new Promise((resolve, reject) => {
+        Hospital.find({ nombre: regex }, (err, hospitales) => {
+            if (err) {
+                reject('Error al cargar hospitales', err);
+            } else {
+                resolve(hospitales);
+            }
+        });
+    })
+}
+
+module.exports = app;
