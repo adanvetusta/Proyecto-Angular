3d71fce944388ac8b2f526ee592e591d90437240
Adán Fernández <47212287+adanvetusta@users.noreply.github.com>
2020-04-12 20:07:06 +0200

    Operaciones CRUD de medicos

--- backend-server/app.js
+++ backend-server/app.js
@@ -26,9 +26,12 @@ var appRoutes = require('./routes/app');
 var usuarioRoutes = require('./routes/usuario');
 var loginRoutes = require('./routes/login');
 var hospitalRoutes = require('./routes/hospital');
+var medicoRoutes = require('./routes/medico');
+
 
 
 // Rutas
+app.use('/medico', medicoRoutes);
 app.use('/hospital', hospitalRoutes);
 app.use('/usuario', usuarioRoutes);
 app.use('/login', loginRoutes);
---
+++ backend-server/routes/medico.js
@@ -0,0 +1,133 @@
+var express = require('express');
+
+var mdAutenticacion = require('../middelwares/autenticacion');
+
+var app = express();
+
+var Medico = require('../models/medico');
+
+/**
+ * Obtener todos los medicoes
+ */
+app.get('/', (req, res, next) => {
+    Medico.find({}, 'nombre email img role').exec(
+        (err, medicos) => {
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    mensaje: 'Error cargando medicos',
+                    errors: err
+                });
+            }
+            res.status(200).json({
+                ok: true,
+                medicos
+            });
+        });
+});
+
+
+/**
+ * Crear un nuevo medico
+ */
+app.post('/', mdAutenticacion.verificaToken, (req, res) => {
+    var body = req.body;
+    var medico = new Medico({
+        nombre: body.nombre,
+        usuario: req.usuario,
+        hospital: body.hospital
+    });
+
+    medico.save((err, medicoGuardado) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'Error al crear medico',
+                errors: err
+            });
+        }
+        res.status(201).json({
+            ok: true,
+            medico: medicoGuardado
+        });
+    });
+});
+
+
+/**
+ * Actualizar medico
+ */
+app.put('/:id', mdAutenticacion.verificaToken, (req, res) => {
+    // Obtenemos el id
+    var id = req.params.id;
+    var body = req.body;
+
+    // Callback: err y el resultado obtenido
+    Medico.findById(id, (err, medico) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al buscar medico',
+                errors: err
+            });
+        }
+        if (!medico) {
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'El medico con el id ' + id + ' no existe',
+                errors: { message: 'No existe un medico con ese ID' }
+            });
+        }
+
+        // Obtenemos datos del medico
+        medico.nombre = body.nombre
+        medico.usuario = req.usuario._id;
+        medico.hospital = body.hospital;
+
+        medico.save((err, medicoGuardado) => {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    mensaje: 'Error al modificar medico',
+                    errors: err
+                });
+            }
+
+            res.status(200).json({
+                ok: true,
+                medico: medicoGuardado
+            });
+        });
+    });
+});
+
+
+/**
+ * Borrar un medico por el id
+ */
+app.delete('/:id', mdAutenticacion.verificaToken, (req, res) => {
+    var id = req.params.id;
+
+    Medico.findByIdAndRemove(id, (err, medicoBorrado) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al borrar medico',
+                errors: err
+            });
+        }
+        if (!medicoBorrado) {
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'No existe un medico con ese ID',
+                errors: { message: 'No existe un medico con ese ID' }
+            });
+        }
+        res.status(200).json({
+            ok: true,
+            medico: medicoBorrado
+        });
+    })
+});
+
+module.exports = app;
