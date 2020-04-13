2528d61a07639b18b74dea89ede4469f7dbca041
Adán Fernández <47212287+adanvetusta@users.noreply.github.com>
2020-04-13 19:30:03 +0200

    Buscando en varias colecciones a la vez con promesas

--- backend-server/routes/busqueda.js
+++ backend-server/routes/busqueda.js
@@ -3,6 +3,7 @@ var app = express();
 
 var Hospital = require('../models/hospital');
 var Medico = require('../models/medico');
+var Usuario = require('../models/usuario');
 
 app.get('/todo/:busqueda', (req, res, next) => {
     var busqueda = req.params.busqueda;
@@ -10,25 +11,63 @@ app.get('/todo/:busqueda', (req, res, next) => {
     // que contenga norte y sea insensible a min y may: /busqueda/i.
     var regex = new RegExp(busqueda, 'i');
 
-    buscarHospitales(regex).then(hospitales => {
-        res.status(200).json({
-            ok: true,
-            hospitales: hospitales
+    Promise.all(
+            [
+                buscarHospitales(regex),
+                buscarMedicos(regex),
+                buscarUsuarios(regex)
+            ])
+        .then(respuestas => {
+            res.status(200).json({
+                ok: true,
+                hospitales: respuestas[0],
+                medicos: respuestas[1],
+                usuarios: respuestas[2]
+            });
         });
-    });
 });
+
+function buscarMedicos(regex) {
+    return new Promise((resolve, reject) => {
+        Medico.find({ nombre: regex })
+            .populate('usuario', 'nombre email')
+            .populate('hospital')
+            .exec((err, medicos) => {
+                if (err) {
+                    reject('Error al cargar medicos', err);
+                } else {
+                    resolve(medicos);
+                }
+            });
+    })
+}
+
+function buscarUsuarios(regex) {
+    return new Promise((resolve, reject) => {
+        Usuario.find({}, 'nombre email role ')
+            .or([{ 'nombre': regex }, { 'email': regex }])
+            .exec((err, usuarios) => {
+                if (err) {
+                    reject('Error al cargar usuarios', err);
+                } else {
+                    resolve(usuarios);
+                }
+            });
+    })
+}
 
 
 function buscarHospitales(regex) {
-
     return new Promise((resolve, reject) => {
-        Hospital.find({ nombre: regex }, (err, hospitales) => {
-            if (err) {
-                reject('Error al cargar hospitales', err);
-            } else {
-                resolve(hospitales);
-            }
-        });
+        Hospital.find({ nombre: regex })
+            .populate('usuario', 'nombre email')
+            .exec((err, hospitales) => {
+                if (err) {
+                    reject('Error al cargar hospitales', err);
+                } else {
+                    resolve(hospitales);
+                }
+            });
     })
 }
 
