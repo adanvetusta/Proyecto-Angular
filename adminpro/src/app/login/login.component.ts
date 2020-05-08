e76836d8cddf66827aad2d9e658845d070df4eee
Adán Fernández <47212287+adanvetusta@users.noreply.github.com>
2020-05-08 23:50:10 +0200

    autenticación con Google: Token
    Obtener token de autenticación

--- adminpro/src/app/login/login.component.ts
+++ adminpro/src/app/login/login.component.ts
@@ -5,6 +5,8 @@ import { UsuarioService } from '../services/usuario/usuario.service';
 import { Usuario } from '../models/usuario.model';
 
 declare function init_plugins();
+// Declaramos libreria gapi
+declare const gapi: any;
 
 @Component({
   selector: 'app-login',
@@ -16,12 +18,38 @@ export class LoginComponent implements OnInit {
   email: string;
   recuerdame: boolean = false;
 
+  auth2;
+
   constructor(public router: Router, public _usuarioService: UsuarioService) { }
 
   ngOnInit(): void {
     init_plugins();
+    this.googleInit();
+    
     this.email = localStorage.getItem('email') || '';
-    this.recuerdame = localStorage.getItem('email').length > 0;
+    this.recuerdame = localStorage.getItem('email') && localStorage.getItem('email').length > 0;
+  }
+
+  /**
+   * Fuente: https://developers.google.com/identity/sign-in/web/listeners
+   */
+  googleInit() {
+    gapi.load('auth2', () => {
+      this.auth2 = gapi.auth2.init({
+        client_id: '790875260017-8rf3qmuq0r4t6loe751a3tg82vjholmi.apps.googleusercontent.com',
+        cookiepolicy: 'single_host_origin',
+        scope: 'profile email'
+      });
+      this.attachSignIn(document.getElementById('btnGoogle'));
+    });
+  }
+
+
+  attachSignIn(elementHtml) {
+    this.auth2.attachClickHandler(elementHtml, {}, googleUser => {
+      const profile = googleUser.getBasicProfile();
+      console.log(profile);
+    });
   }
 
 
