6fafab28214d678c01bd603ccd2b479c765fac48
Adán Fernández <47212287+adanvetusta@users.noreply.github.com>
2020-05-21 19:06:35 +0200

    Servicios de hospitales

--- adminpro/src/app/services/hospital/hospital.service.ts
+++ adminpro/src/app/services/hospital/hospital.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {URL_SERVICE} from '../../config/config';
 import {map} from 'rxjs/operators';
+import {UsuarioService} from '../usuario/usuario.service';
+import {Hospital} from '../../models/hospital.model';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +12,7 @@ export class HospitalService {
 
   totalHospitales: number = 0;
 
-  constructor(public http: HttpClient) { }
+  constructor(public http: HttpClient, public _usuarioService: UsuarioService) { }
 
   cargarHospitales() {
     const url = URL_SERVICE + '/hospital';
@@ -26,6 +28,41 @@ export class HospitalService {
     const url = URL_SERVICE + '/hospital/' + id;
     return this.http.get(url).pipe(
       map((res: any) => res.hospital)
+    );
+  }
+
+   borrarHospital(id: string) {
+      const url = URL_SERVICE + '/hospital/' + id + '?token=' + this._usuarioService.token;
+      return this.http.delete(url).pipe(
+        map(res => {
+          console.log('Hospital borrado');
+        })
+      );
+   }
+
+   crearHospital(nombre: string) {
+      const url = URL_SERVICE + '/hospital?token=' + this._usuarioService.token;
+      return this.http.post(url, {nombre}).pipe(
+        map(
+          (res: any) =>  res.hospital
+        )
+      );
+   }
+
+  buscarHospital(termino: string) {
+    const url = URL_SERVICE + '/busqueda/coleccion/hospitales/' + termino;
+    return this.http.get(url).pipe(
+      map( (res: any) => res.hospitales
+      )
+    );
+  }
+
+  actualizarHospital(hospital: Hospital) {
+    const url = URL_SERVICE + '/hospital/' + hospital._id + '/?token=' + this._usuarioService.token;
+    return this.http.put(url, hospital).pipe(
+      map(
+        (res: any) =>  res.hospital
+      )
     );
   }
 }
