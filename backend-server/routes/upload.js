6ef48be745ee72b8888002599c9928f9f016c0c3
Adán Fernández <47212287+adanvetusta@users.noreply.github.com>
2020-05-22 23:18:12 +0200

    Error back solucionado

--- backend-server/routes/upload.js
+++ backend-server/routes/upload.js
@@ -126,7 +126,7 @@ function subirPorTipo(tipo, id, nombreArchivo, res) {
                 return res.status(200).json({
                     ok: true,
                     mensaje: 'Imagen de hospital subida/actualizada',
-                    medico: hospitalActualizado
+                    hospital: hospitalActualizado
                 });
             })
         });
