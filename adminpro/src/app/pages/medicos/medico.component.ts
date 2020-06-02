0c6f27746696443381fcf921a3efe8be584c7e14
Adán Fernández <47212287+adanvetusta@users.noreply.github.com>
2020-06-02 17:39:44 +0200

    Mostrar información de un hospital preseleccionado

--- adminpro/src/app/pages/medicos/medico.component.ts
+++ adminpro/src/app/pages/medicos/medico.component.ts
@@ -5,6 +5,7 @@ import {HospitalService} from '../../services/hospital/hospital.service';
 import {Medico} from '../../models/medico.model';
 import {MedicoService} from '../../services/medico/medico.service';
 import Swal from 'sweetalert2';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-medico',
@@ -15,12 +16,15 @@ export class MedicoComponent implements OnInit {
 
 
   hospitales: Hospital[] = [];
-  medico: Medico = new Medico();
-  constructor(public _hospitalService: HospitalService, public _medicoService: MedicoService) { }
+  medico: Medico = new Medico('', '', '', null, '');
+  hospital: Hospital = new Hospital('');
+
+  constructor(public _hospitalService: HospitalService, public _medicoService: MedicoService, public router: Router, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
     this._hospitalService.cargarHospitales().subscribe( hospitales => {
       this.hospitales = hospitales;
+      this.medico._id =  this.activatedRoute.snapshot.params.id !== 'nuevo' ? this.activatedRoute.snapshot.params.id  : null;
     });
   }
 
@@ -30,6 +34,14 @@ export class MedicoComponent implements OnInit {
     }
     this._medicoService.guardarMedico(this.medico).subscribe(medico => {
       Swal.fire('Médico creado', 'Operación realizada correctamente', 'success');
+      this.medico._id = medico._id;
+      this.router.navigate(['/medico', medico._id]);
+    });
+  }
+
+  cambioHospital(id: string) {
+    this._hospitalService.obtenerHospital(id).subscribe( hospital => {
+      this.hospital = hospital;
     });
   }
 }
